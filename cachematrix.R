072fc7dcfab42858264d403eb4fc1ea33db31317
Adam Koonce <adamkoonce@me.com>
2019-07-28 16:34:33 -0600

    Changes to makeCacheMatrix and cacheSolve in line with the programming assignment requirements.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve can be used in conjunction to 
+## cache the results of a Solve() over a given matrix, so subsequent
+## operations over the same matrix can be retrieved from cache.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list for a given matrix that includes the functions
+## to cache and retrieve the results.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setSolve <- function(solve) m <<- solve
+  getSolve <- function() m
+  list(set = set, get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve uses the functions attached to the matrix using makeCacheMatrix
+## to either retrieve the cached result, or Solve the matrix and save the result
+## to cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getSolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setSolve(m)
+  m
 }
